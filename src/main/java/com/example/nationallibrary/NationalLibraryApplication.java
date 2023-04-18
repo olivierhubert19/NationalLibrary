c3ac4a4d69633ebfd38f839f5121a76ef5cdec8c
olivierhubert19 <dothanhcong1932002@gmail.com>
2023-04-18 16:37:35 +0700

    first

---
+++ src/main/java/com/example/nationallibrary/NationalLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.nationallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationalLibraryApplication.class, args);
+    }
+
+}
