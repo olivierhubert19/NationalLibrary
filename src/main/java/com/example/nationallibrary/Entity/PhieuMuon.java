0270cf30d4747acbccb9feafa906af5f42091925
TuanVH <tuanvh@paditech.com>
2023-05-10 07:27:09 +0700

    fix sql

--- src/main/java/com/example/nationallibrary/Entity/PhieuMuon.java
+++ src/main/java/com/example/nationallibrary/Entity/PhieuMuon.java
@@ -11,16 +11,15 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name="PhieuMuon")
 public class PhieuMuon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+
     private int id;
-    @Column(name = "id_Reader")
-    private int idReader;
-    @Column(name = "ngay_Muon")
+
+    private Integer idReader;
+
     private Date ngayMuon;
-    @Column(name = "tinh_Trang")
-    private boolean tinhTrang;
+
+    private Boolean tinhTrang;
 }
