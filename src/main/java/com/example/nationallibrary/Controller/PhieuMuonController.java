eb4b0e343245308007c3a8c80b96cfc95cf56ecb
olivierhubert19 <dothanhcong1932002@gmail.com>
2023-05-10 22:55:43 +0700

    chi tiet phieu muon admin

--- src/main/java/com/example/nationallibrary/Controller/PhieuMuonController.java
+++ src/main/java/com/example/nationallibrary/Controller/PhieuMuonController.java
@@ -1,8 +1,10 @@
 package com.example.nationallibrary.Controller;
 
+import com.example.nationallibrary.Entity.Book;
 import com.example.nationallibrary.Entity.BorowedBook;
 import com.example.nationallibrary.Entity.PhieuMuon;
 import com.example.nationallibrary.Entity.PhieuTra;
+import com.example.nationallibrary.Service.BookService;
 import com.example.nationallibrary.Service.BorowedBookService;
 import com.example.nationallibrary.Service.PhieuMuonService;
 import com.example.nationallibrary.Service.PhieuTraService;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -24,11 +27,20 @@ public class PhieuMuonController {
     public PhieuTraService phieuTraService;
     @Autowired
     public BorowedBookService borowedBookService;
+    @Autowired
+    public BookService bookService;
 
     @GetMapping("/admin/chitietPhieuMuon/{id}")
     public String chiTietPhieuMuon(@PathVariable("id") int id, Model model){
-        PhieuMuon pm = phieuMuonService.getPhieuMuonById(id);
-        model.addAttribute("pm",pm);
+        PhieuMuon p = phieuMuonService.getPhieuMuonById(id);
+        model.addAttribute("phieumuon", p);
+        List<BorowedBook> tmp = borowedBookService.getByIdPhieuMuon(id);
+        List<Book> list = new ArrayList<>();
+        for(int i=0;i<tmp.size();i++){
+            Book b = bookService.getBookById(tmp.get(i).getIdBook());
+            if(b!=null) list.add(b);
+        }
+        model.addAttribute("list", list);
         return "adminChiTietPhieuMuon";
     }
 
